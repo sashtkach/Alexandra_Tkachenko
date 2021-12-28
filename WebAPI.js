9d73ce7f90bcf81993d98893b6ff399c65bd0af8
Alexandra Tkachenko <39911468+sashtkach@users.noreply.github.com>
2021-12-28 05:32:33 +0200

    Add files via upload

---
+++ WebAPI.js
@@ -0,0 +1,80 @@
+XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+
+class TestWebAPI
+{
+	Upload()
+	{
+		var data = "text";
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener("readystatechange", function() {
+		  if(this.readyState === 4) {
+			console.log(this.responseText);
+		  }
+		});
+
+		xhr.open("POST", "https://content.dropboxapi.com/2/files/upload");
+		xhr.setRequestHeader("Dropbox-API-Arg", "{\"path\": \"/New_file.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}");
+		xhr.setRequestHeader("Content-Type", "application/octet-stream");
+		xhr.setRequestHeader("Authorization", "Bearer 225EFbzpYXsAAAAAAAAAAXBb167PH4qy61f7PMsJucrKWrSgPjRURPBcUD-tJjBT");
+
+		xhr.send(data);
+	}
+
+
+
+	GetFileMetadata()
+	{
+		var data = JSON.stringify({
+		  "file": "/New_file.txt",
+		  "actions": []
+		});
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener("readystatechange", function() {
+		  if(this.readyState === 4) {
+			console.log(this.responseText);
+		  }
+		});
+
+		xhr.open("POST", "https://api.dropboxapi.com/2/sharing/get_file_metadata");
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.setRequestHeader("Authorization", "Bearer 225EFbzpYXsAAAAAAAAAAXBb167PH4qy61f7PMsJucrKWrSgPjRURPBcUD-tJjBT");
+
+		xhr.send(data);
+	}
+
+
+	DeleteFile()
+	{
+		var data = JSON.stringify({
+		  "path": "/New_file.txt"
+		});
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener("readystatechange", function() {
+		  if(this.readyState === 4) {
+			console.log(this.responseText);
+		  }
+		});
+
+		xhr.open("POST", "https://api.dropboxapi.com/2/files/delete_v2");
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.setRequestHeader("Authorization", "Bearer 225EFbzpYXsAAAAAAAAAAXBb167PH4qy61f7PMsJucrKWrSgPjRURPBcUD-tJjBT");
+
+		xhr.send(data);
+	}
+}
+
+Test=new TestWebAPI();
+
+Test.Upload();
+Test.GetFileMetadata();
+Test.DeleteFile();
